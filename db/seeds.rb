5a005929d765be18eb797dd2904d1db590ac1bd4
Rafael1981 <r.a.ferreira1981@gmail.com>
2018-09-03 10:13:55 +1000

    fixing variables names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-10.times do |x|
+10.times do |blog|
 	Blog.create!(
-		title: "My Blog Post#{x}",
+		title: "My Blog Post#{blog}",
 		body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
 	)
 end
 
 puts "10 blog posts created"
 
-5.times do |x|
+5.times do |skill|
 	Skill.create!(
-		title: "Rails #{x}",
+		title: "Rails #{skill}",
 		percent_utilized: 20
 		)
 end
 
 puts "5 skills created"
 
-9.times do |x|
+9.times do |portfolio|
 	Portfolio.create!(
-		title: "Portfolio title: #{x}",
+		title: "Portfolio title: #{portfolio}",
 		subtitle: "My greate service",
 		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
 		main_image: "https://via.placeholder.com/600x400" ,
