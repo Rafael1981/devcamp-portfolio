a535a46a34703147124a619e1ade6edfb39377eb
Rafael1981 <r.a.ferreira1981@gmail.com>
2018-09-04 15:06:50 +1000

    Integrating edit feature  to Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,8 +8,6 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
-
-
 	def create
 		@portfolio_item = Portfolio.new(portfolio_params)
 
@@ -19,9 +17,23 @@ class PortfoliosController < ApplicationController
 			else				
 				format.html { render :new}
 			end
-
 		end
+	end
 
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
 	end
 
 
