5b140ea97bf93812f6ef319cb32a404c82ad04dc
Rafael1981 <r.a.ferreira1981@gmail.com>
2018-09-26 09:14:02 +1000

    Implementing Custom Scopes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,15 @@ class PortfoliosController < ApplicationController
 
 	def index
 		@portfolio_items = Portfolio.all
+	# scope demos:
+	# 	@portfolio_items = Portfolio.angular
+	# 	@portfolio_items = Portfolio.ruby_on_rails_port_items
 	end
+
+	def angular
+		@angular_portfolio_items = Portfolio.angular
+	end
+
 
 	def new
 		@portfolio_item = Portfolio.new
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,15 @@
 class Portfolio < ApplicationRecord
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+
+
+# forms of using scope
+  #creating a ruby method
+	def self.angular
+		where(subtitle: 'Angular')
+	end
+#using an actual scope (prefer form, as it uses less code)
+	scope :ruby_on_rails_port_items, -> {where(subtitle: 'Ruby on Rails')}
 end
+
